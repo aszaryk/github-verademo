d667144cd016c969d0783c466b3ef51b84adf426
Andrzej Szaryk <aszaryk@veracode.com>
2021-05-27 10:03:25 -0400

    changed toolscontroller flaw

--- src/main/java/com/veracode/verademo/controller/ToolsController.java
+++ src/main/java/com/veracode/verademo/controller/ToolsController.java
@@ -53,7 +53,7 @@ public class ToolsController {
 		try {
 			/* START BAD CODE */
 			if (System.getProperty("os.name").startsWith("Windows")) {
-				proc = Runtime.getRuntime().exec("cmd.exe /c ping " + host);
+				proc = Runtime.getRuntime().exec("cmd.exe /c ping " + "http://testserver");
 			}
 			else {
 				proc = Runtime.getRuntime().exec("ping " + host);
