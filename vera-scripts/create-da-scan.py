4809f81d6acf6983f21c03d2e697e6dc8c8fe658
Andrzej Szaryk <aszaryk@veracode.com>
2023-06-27 16:56:40 -0400

    python scripts

--- vera-scripts/create-da-scan.py
+++ vera-scripts/create-da-scan.py
@@ -15,36 +15,36 @@ from veracode_api_py import VeracodeAPI as vapi
 
 
 def main():
-#Setup variables according to environment
+    #Setup variables according to environment
 
-#GitHub:
-api_id = '${{ secrets.VERACODE_API_ID }}'
-api_secret = '${{ secrets.VERACODE_API_KEY }}'
-dynamic_job =  'GitHub Test Scan'    #'${{ github.repository }}' #Dynamic Job name will be same as GitHub project name
+    #GitHub:
+    api_id = '${{ secrets.VERACODE_API_ID }}'
+    api_secret = '${{ secrets.VERACODE_API_KEY }}'
+    dynamic_job =  'GitHub Test Scan'    #'${{ github.repository }}' #Dynamic Job name will be same as GitHub project name
 
-#Payload for creating and scheduling new DA job
-data =   {
-  "name": dynamic_job,
-  "scans": [
-    {
-      "scan_config_request": {
-        "target_url": {
-          "url": "http://my.verademo.site"
+    #Payload for creating and scheduling new DA job
+    data =   {
+      "name": dynamic_job,
+      "scans": [
+        {
+          "scan_config_request": {
+            "target_url": {
+              "url": "http://my.verademo.site"
+            }
+          }
+        }
+      ],
+      "schedule": {
+        "now": True,
+        "duration": {
+          "length": 1,
+          "unit": "DAY"
         }
       }
     }
-  ],
-  "schedule": {
-    "now": True,
-    "duration": {
-      "length": 1,
-      "unit": "DAY"
-    }
-  }
-}
 
-analyses = vapi.get_analyses()
-print(analyses)
+    analyses = vapi.get_analyses()
+    print(analyses)
 
 if __name__ == "__main__":
     main()
