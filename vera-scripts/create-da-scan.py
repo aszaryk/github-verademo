7f60fc90f8fc255d84e94369eda150adff25e146
Andrzej Szaryk <aszaryk@veracode.com>
2023-06-27 17:09:18 -0400

    python scripts

--- vera-scripts/create-da-scan.py
+++ vera-scripts/create-da-scan.py
@@ -8,7 +8,7 @@ import sys
 #from hashlib import sha256
 import requests
 from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
-from veracode_api_py import VeracodeAPI as vapi
+from veracode_api_py.dynamic import Analyses, Scans, ScanCapacitySummary, ScanOccurrences, ScannerVariables, DynUtils, Occurrences
 
 #from requests.adapters import HTTPAdapter
 #from urllib.parse import urlparse
@@ -43,8 +43,29 @@ def main():
       }
     }
 
-    analyses = vapi.get_analyses()
-    print(analyses)
+    url = DynUtils().setup_url('http://www.example.com','DIRECTORY_AND_SUBDIRECTORY',False)
+
+    allowed_hosts = [url]
+
+    auth = DynUtils().setup_auth('AUTO','admin','smithy')
+
+    auth_config = DynUtils().setup_auth_config(auth)
+
+    crawl_config = DynUtils().setup_crawl_configuration([],False)
+
+    scan_setting = DynUtils().setup_scan_setting(blocklist_configs=[],custom_hosts=[],user_agent=None)
+
+    scan_config_request = DynUtils().setup_scan_config_request(url, allowed_hosts,auth_config, crawl_config, scan_setting)
+
+    scan_contact_info = DynUtils().setup_scan_contact_info('tjarrett@example.com','Alan Smithee','800-555-1212')
+
+    scan = DynUtils().setup_scan(scan_config_request,scan_contact_info)
+
+    print(scan)
+
+    analysis = Analyses().create('My API Analysis 4',scans=[scan],owner='Tim Jarrett',email='tjarrett@example.com')
+
+    print(analysis)
 
 if __name__ == "__main__":
     main()
