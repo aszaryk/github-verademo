f99b06ff258ad72ca02b19188470ba9e86dee920
Andrzej Szaryk <aszaryk@veracode.com>
2023-06-27 16:54:27 -0400

    python scripts

--- vera-scripts/create-da-scan.py
+++ vera-scripts/create-da-scan.py
@@ -1,61 +1,26 @@
 #!/usr/bin/env python3
-import os
-import time                                                                     
-import hmac                                                                     
-import codecs
+#import os
+#import time
+#import hmac
+#import codecs
 import json  
 import sys                                                              
-from hashlib import sha256 
+#from hashlib import sha256
 import requests
-from requests.adapters import HTTPAdapter                                       
-from urllib.parse import urlparse
+from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
+from veracode_api_py import VeracodeAPI as vapi
 
+#from requests.adapters import HTTPAdapter
+#from urllib.parse import urlparse
+
+
+def main():
 #Setup variables according to environment
 
 #GitHub:
 api_id = '${{ secrets.VERACODE_API_ID }}'
 api_secret = '${{ secrets.VERACODE_API_KEY }}'
-dynamic_job = '${{ github.repository }}' #Dynamic Job name will be same as GitHub project name
-
-
-def veracode_hmac(host, url, method):
-    signing_data = 'id={api_id}&host={host}&url={url}&method={method}'.format(
-                    api_id=api_id.lower(),
-                    host=host.lower(),
-                    url=url, method=method.upper())
-
-    timestamp = int(round(time.time() * 1000))
-    nonce = os.urandom(16).hex()
-
-    key_nonce = hmac.new(
-        codecs.decode(api_secret, 'hex_codec'),
-        codecs.decode(nonce, 'hex_codec'), sha256).digest()
-
-    key_date = hmac.new(key_nonce, str(timestamp).encode(), sha256).digest()
-    signature_key = hmac.new(
-            key_date, 'vcode_request_version_1'.encode(), sha256).digest()
-    signature = hmac.new(
-            signature_key, signing_data.encode(), sha256).hexdigest()
-
-    return '{auth} id={id},ts={ts},nonce={nonce},sig={sig}'.format(
-            auth='VERACODE-HMAC-SHA-256',
-            id=api_id,
-            ts=timestamp,
-            nonce=nonce,
-            sig=signature)
-
-def prepared_request(method, end_point, json=None, query=None, file=None):
-    session = requests.Session()
-    session.mount(end_point, HTTPAdapter(max_retries=3))
-    request = requests.Request(method, end_point, json=json, params=query, files=file)
-    prepared_request = request.prepare()
-    prepared_request.headers['Authorization'] = veracode_hmac(
-        urlparse(end_point).hostname, prepared_request.path_url, method)
-    res = session.send(prepared_request)
-
-    return res
-
-# code above this line is reusable for all/most API calls
+dynamic_job =  'GitHub Test Scan'    #'${{ github.repository }}' #Dynamic Job name will be same as GitHub project name
 
 #Payload for creating and scheduling new DA job
 data =   {
@@ -78,12 +43,8 @@ data =   {
   }
 }
 
+analyses = vapi.get_analyses()
+print(analyses)
 
-print("Creating a new Dynamic Analysis Job: " + dynamic_job )
-res = prepared_request('POST', 'https://api.veracode.com/was/configservice/v1/analyses', json=data)
-
-if res.status_code == 201:
-    print("Job Created and Submitted Successfully: " + str(res.status_code))
-else:
-    response = res.json()
-    print("Error encountered: " + response['_embedded']['errors'][0]['detail'])
+if __name__ == "__main__":
+    main()
